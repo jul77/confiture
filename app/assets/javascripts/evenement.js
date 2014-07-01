68e1fa1ecd3fdeac84a4b3e43fe8c0450e77b688
jul77 <juleserbin@gmail.com>
2014-07-01 11:45:25 +0200

    integration

---
+++ app/assets/javascripts/evenement.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$('.button_account').on('click', function() {
+		$('.content_invisible_layout').slideDown('slow', function(){alert('je suis un putain de singe')});
+	});
+
+
+})
