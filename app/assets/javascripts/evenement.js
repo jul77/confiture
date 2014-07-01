c33833467f1bb06da460f80c766cff0cb09194ac
jul77 <juleserbin@gmail.com>
2014-07-01 14:11:12 +0200

    integration menu

--- app/assets/javascripts/evenement.js
+++ app/assets/javascripts/evenement.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
 	$('.button_account').on('click', function() {
-		$('.content_invisible_layout').slideDown('slow', function(){alert('je suis un putain de singe')});
+
+		$('.invisible_layout').slideToggle('slow', function(){
+			$('.content_account').toggle('slow');
+		
+		});
 	});
 
 
