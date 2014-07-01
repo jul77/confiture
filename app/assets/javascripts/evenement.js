7b52539a52471245208e68070a77debfde8fd017
jul77 <juleserbin@gmail.com>
2014-07-01 16:47:55 +0200

    :icecream: :it: :koala:

--- app/assets/javascripts/evenement.js
+++ app/assets/javascripts/evenement.js
@@ -4,9 +4,22 @@ $(document).ready(function(){
 
 		$('.invisible_layout').slideToggle('slow', function(){
 			$('.content_account').toggle('slow');
-		
+			$('.content_subscribe').toggle('slow');
+			console.log('ok');
 		});
-	});
+	})
+
+	$('.subscribe_button').on('click', function() {
+		$('.content_account').animate({top: '-100%'},500);
+		$('.invisible_layout').animate({height: '470px'}, 500);
+		$('.content_subscribe').animate({top: '-74%'},500);
+	})
+		
+	$('.account_button').on('click', function() { 
+		$('.content_subscribe').animate({top: 0},500);
+		$('.invisible_layout').animate({height: '320px'}, 500);
+		$('.content_account').animate({top: 0},500);
+	})
 
 
 })
